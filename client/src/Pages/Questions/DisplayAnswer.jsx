9072c563da3da9367fcd72540667e4a0bf1c05b3
Devansh Shah <shahdevansh949@gmail.com>
2023-09-02 20:17:05 +0530

    Added Delete Question and answer functionalities and minor bug fixes

--- client/src/Pages/Questions/DisplayAnswer.jsx
+++ client/src/Pages/Questions/DisplayAnswer.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import moment from 'moment'
 import Avatar from '../../components/Avatar/Avatar'
+import { useDispatch, useSelector } from 'react-redux'
+import { deleteAnswer } from '../../actions/question'
 
 
 const DisplayAnswer = ({question, handleShare}) => {
+    const User = useSelector((state)=>(state.currentUserReducer))
+    const dispatch = useDispatch()
+    const {id} = useParams()
+    const handleDelete = (answerId, noOfAnswers)=>{
+        dispatch(deleteAnswer(id, answerId, noOfAnswers - 1))
+    }
   return (
     <div>
         {
@@ -14,7 +22,11 @@ const DisplayAnswer = ({question, handleShare}) => {
                     <div className="question-actions-user">
                         <div>
                             <button type='button' onClick={handleShare}>Share</button>
-                            <button type='button'>Delete</button>
+                            {
+                                User?.result?._id === question?.userId && (
+                                    <button type='button' onClick={()=> handleDelete(ans._id, question.noOfAnswers )}>Delete</button>
+                                )
+                            }
                         </div>
                         <div>
                             <p>answered {moment(ans.answeredOn).fromNow()}</p>
--- server/controllers/Questions.js
+++ server/controllers/Questions.js
@@ -3,8 +3,7 @@ import mongoose from 'mongoose'
 
 export const AskQuestion=async  (req,res)=>{
     const postQuestionData=req.body;
-
-    const postQuestion=new Questions({...postQuestionData,userId:req.userId})
+    const postQuestion=new Questions(postQuestionData)
     try{
         await postQuestion.save()
         res.status(200).json('Posted a Question successfully')
@@ -21,6 +20,19 @@ export const getAllQuestions=async(req,res)=>{
         res.status(200).json(questionList)
 
     }catch(error){
+        res.status(404).json({message:error.message})
+    }
+}
+
+export const deleteQuestion = async(req,res)=>{
+    const {id:_id} = req.params
+    if(!mongoose.Types.ObjectId.isValid(_id)){
+        return res.status(404).send('No Such Question Availble...')
+    }
+    try {
+        await Questions.findByIdAndRemove(_id)
+        res.status(200).json({message:"Successfully Deleted Question"})
+    } catch (error) {
         res.status(404).json({message:error.message})
     }
 }
